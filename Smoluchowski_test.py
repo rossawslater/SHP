15e5d9e679d4191ae42ab327fa603440b5e13e56
Ross Slater <rossawslater@gmail.com>
2019-02-28 17:15:20 +0000

    Begin Editing code so data can be plotted from file

--- Smoluchowski_test.py
+++ Smoluchowski_test.py
@@ -5,9 +5,10 @@ import sys
 
 class Smoluchowski():
 	"""Simulator using Smoluchowski Eqn"""
-	def __init__(self, init_no_particles, sim_length, max_agg_size = 5, k = 6E-4,):
+	def __init__(self, init_no_particles, sim_length, max_agg_size = 5, k = 6E-4):
 		self.init_no_particles = init_no_particles
 		self.sim_length = sim_length #number of time steps for simulation
+		# self.k = 3.167E-3 #Literature value of D for Pa
 		self.k = k
 		self.max_agg_size = (max_agg_size + 1)#+1 so we can index from 1 not 0
 		# self.current_max_agg_size = 2 #need to adjust length of data array if using this?
@@ -25,11 +26,13 @@ class Smoluchowski():
 		self.aggregate_count = self.init_no_particles #count of aggregates and particles in system is initallised as initial no particles
 		self.particle_count = self.init_no_particles
 
-		self.dt = 0.1
+		# self.dt = 0.1
+		self.dt = 0.05
 
 		self.weighted_data = np.zeros((self.sim_length, self.max_agg_size))
 
-		self.gavins_times = [103,137,162,201,252,297]
+		# self.gavins_times = [103,137,162,201,252,297]
+		self.gavins_times = range(0,25,1)
 
 	def produce(self,j,i):
 		self.aggregates[j] += (self.k/2 * (self.aggregates[i]*self.aggregates[j-i]*self.dt))
@@ -41,15 +44,15 @@ class Smoluchowski():
 			self.aggregates[j] -= (self.k*self.aggregates[j]*self.aggregates[i]*self.dt)
 
 	def grow(self):
-		self.aggregates[1] += 1.023*self.dt*self.aggregates[1]
+		self.aggregates[1] += 1.023*self.dt*self.aggregates[1]*10
 		# self.aggregates[1] = self.init_no_particles
 
 	def get_j_counts_at_step(self,j,t):
-		self.data[t,j]=self.aggregates[j]*j/self.init_no_particles
+		# self.data[t,j]=self.aggregates[j]*j/self.init_no_particles
 		self.data[t,j]=self.aggregates[j]/self.init_no_particles
 
 	def get_weighted_j_counts_at_step(self,j,t): #dont ned this as already probablility?
-		self.weighted_data[t,j] = (self.aggregates[j]*j)/self.init_no_particles
+		self.weighted_data[t,j] = (self.aggregates[j]*j)#/self.init_no_particles
 
 	def get_aggregate_count(self):
 		self.current_no_aggregates.append(self.aggregate_count/self.init_no_particles)
@@ -87,8 +90,8 @@ class Smoluchowski():
 				self.count_total_particles(j)
 
 			self.time.append(t*self.dt)
+			self.data[t,0] = t*self.dt
 			print t*self.dt
-
 
 	def run_analytical(self):
 		for t in range(0,self.sim_length):
@@ -105,24 +108,22 @@ class Smoluchowski():
 			print(i[1]/self.init_no_particles)
 
 	def plot(self):
-		for i in range(1,self.max_agg_size):
+		for i in range(1,len(self.data[0])):#self.max_agg_size):
 			# print(self.data[:,i])
-			print "\n", np.argmax(self.data[:,i])
-			plt.plot(self.time,self.data[:,i], label = "N%i"%(i))
-			# plt.plot(self.time,self.weighted_data[:,i], label = "N%i"%(i))
-		# plt.plot(self.time, self.current_no_aggregates, label = "Total no. of aggregates") #no. aggregates as func time
-		# plt.plot(self.time, self.current_no_particles, label = "Total number of particles") #should be horizonta line at 1 but is decaying
+			# print "\n", np.argmax(self.data[:,i])
+			print len(self.time), len(self.data[:,i])
+			# plt.plot(self.time,self.data[:,i], label = "N%i"%(i))
 
 		plt.xlabel("t")
 		plt.ylabel("N/N Initial")
 		plt.title("Smoluchowski Aggregation Model")
 		x1,x2,y1,y2 = plt.axis()
-		# plt.axis((x1,x2,y1,1.1))
+		plt.axis((x1,x2,y1,1.1))
 		# plt.legend(bbox_to_anchor=(0.5, 0.8), loc=2, borderaxespad=0.)
 		plt.show()
 
 		for i in range(1,self.max_agg_size):
-			plt.plot(self.time,self.weighted_data[:,i], label = "N%i"%(i))
+			plt.plot(self.weighted_data[:,0],self.weighted_data[:,i], label = "N%i"%(i))
 		# plt.plot(self.time, self.current_no_aggregates, label = "Total no. of aggregates") #no. aggregates as func time
 		# plt.plot(self.time, self.current_no_particles, label = "Total number of particles") #should be horizonta line at 1 but is decaying
 
@@ -130,41 +131,43 @@ class Smoluchowski():
 		plt.ylabel("N/N Initial")
 		plt.title("Smoluchowski Aggregation Model")
 		x1,x2,y1,y2 = plt.axis()
-		# plt.axis((x1,x2,y1,1.1))
+		# plt.axis((x1,x2,y1,y2))
 		# plt.legend(bbox_to_anchor=(0.5, 0.8), loc=2, borderaxespad=0.)
 		plt.show()
-
-	def find_peaks(self):
-		self.peaks = np.zeros(self.max_agg_size)
-		for j in range(1,self.max_agg_size):
-			self.peaks[j] = np.argmax(self.data[:,j])
-
-		print self.peaks
 
 	def plot_hist(self):
 		x = 0
 		for i in self.gavins_times:
-			print i
-			print self.data[(i*10)]
-			print self.time[(i*10)]
+			# print i
+			# print self.data[(i*10)]
+			# print self.time[(i*10)]
 			# plt.plot(self.data[(i*10),:], label = "N%i"%(i))
 			plt.plot(self.weighted_data[(i*10),:], label = "N%i"%(i))
 			x1,x2,y1,y2 = plt.axis()
-			plt.axis((1,x2,y1,1))
+			plt.axis((1,200,y1,2000))
 			plt.title("Distribution of Aggregates at %i Minutes"%(i))
-			plt.ylabel("Probability of Bacteria being in Agg of size N")
+			plt.ylabel("Number of Bacteria in Agg of size N")
 			plt.xlabel("Aggregate Size N")
-			# plt.savefig("Peak at N%i.pdf"%(i))
+			plt.savefig("Growth_Distribution_at_%i_mins.png"%(i))
 			plt.show()
 			print np.sum(self.data[(i*10),1:]) #total probablility not conserved
 			x += np.sum(self.data[(i*10),1:])
 		print x
 
+	def load_data(self,file):
+		self.data = np.loadtxt(file)
+
+	def load_weighted_data(self,file):
+		self.weighted_data = np.loadtxt(file)
+
 def main():
-	sim = Smoluchowski(200, 3000, 100)
-	sim.run_sim()
+	sim = Smoluchowski(20000, 6000, 10)
+	sim.load_data("Reaggregation_data.txt")
+	sim.load_weighted_data("Reaggregatoin_weighted_data.txt")
+	# sim.run_sim()
 	# sim.run_analytical()
 	sim.plot()
-	sim.find_peaks()
 	sim.plot_hist()
+	# np.savetxt("Reaggregation_data.txt", sim.data)
+	# np.savetxt("Reaggregatoin_weighted_data.txt", sim.weighted_data)
 main()
